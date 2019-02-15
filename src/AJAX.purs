8431481b53e6cc79593e8245af9e68e0d046dcf4
Omar Mefire <omefire@gmail.com>
2019-02-15 18:12:20 +0100

    Moved AJAX calls to a separate module

--- src/AJAX.purs
+++ src/AJAX.purs
@@ -4,6 +4,7 @@ import Prelude
 
 import Simple.JSON as JSON
 import Data.Argonaut.Core as J
+-- import Data.Argonaut.Core (Json)
 import Affjax as AX
 import Effect.Aff
 import Data.Either
@@ -11,23 +12,28 @@ import Data.HTTP.Method
 import Affjax.ResponseFormat as ResponseFormat
 import Data.List.NonEmpty
 import Foreign (ForeignError(..), MultipleErrors)
+import Data.Maybe
+import Affjax.RequestBody as ReqBody
 
-makeRequest :: forall a. (JSON.ReadForeign a) => String -> Aff (Either String a)
-makeRequest url = do
-  res <- AX.request ( AX.defaultRequest { url = url, method = Left GET, responseFormat = ResponseFormat.json } )
+makeRequest :: forall a b. (JSON.ReadForeign a) => String -> Method -> Maybe J.Json -> Aff (Either String a)
+makeRequest url method (Just content) = do
+  res <- AX.request ( AX.defaultRequest { url = url, method = Left method, responseFormat = ResponseFormat.json, content = Just (ReqBody.Json content) } )
   case res.body of
-    Left err -> do
-      -- liftEffect $ Helpers.alert $ AX.printResponseFormatError err
-      pure $ Left $ AX.printResponseFormatError err
-
+    Left err -> pure $ Left $ AX.printResponseFormatError err
     Right json -> do
-      -- _ <- liftEffect $ Helpers.alert $ J.stringify json
       case (JSON.readJSON (J.stringify json)) of
-        Left err -> do
-          let errorStr = getErrorString err
-          pure $ Left $ "An error occured while making a request to URL: " <> url <> ". " <> errorStr
-
+        Left err -> pure $ Left $ "An error occured while making a request to URL: " <> url
         Right (result) -> pure $ Right result
+
+makeRequest url method Nothing = do
+  res <- AX.request ( AX.defaultRequest { url = url, method = Left method, responseFormat = ResponseFormat.json, content = Nothing } )
+  case res.body of
+    Left err -> pure $ Left $ AX.printResponseFormatError err
+    Right json -> do
+      case (JSON.readJSON (J.stringify json)) of
+        Left err -> pure $ Left $ "An error occured while making a request to URL: " <> url
+        Right (result) -> pure $ Right result
+
 
 getErrorString :: NonEmptyList ForeignError -> String
 getErrorString errors = foldl (\str error ->
